b3478f78b46e736a16fa10f18b100d8f8b98df20
keroppiii <fatimah.nurmawati@mhs.unsoed.ac.id>
2025-05-08 21:26:38 +0700

    update

--- app/Models/Departemen.php
+++ app/Models/Departemen.php
@@ -9,10 +9,10 @@ class Departemen extends Model
 {
     use HasFactory;
 
-    // Nama tabel yang digunakan (opsional kalau nama tabel = jamak dari nama model)
+
     protected $table = 'departemens';
 
-    // Kolom-kolom yang bisa diisi secara massal
+
     protected $fillable = [
         'nama_departemen', 
         'kode',
